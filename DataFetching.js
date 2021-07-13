f7faf886d1a63779fc50e022b1ac052f8efaa743
nareshsasi <87058882+nareshsasi@users.noreply.github.com>
2021-07-13 16:00:07 +0530

    Add files via upload

---
+++ DataFetching.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+function DataFetching() {
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+            console.log(res)
+            setPosts(res.data)
+        })
+    }, [])
+    return(
+        <div>
+            <ul> {posts.map(post => (
+                <li key={post.id}>{post.title}</li>
+            ))}
+            </ul>
+        </div>
+    )
+}
+export default DataFetching
